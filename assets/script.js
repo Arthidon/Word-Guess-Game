f9b983b07ca8fa8109cff36c3b7a05938d0e86d6
Anthony Yonta <atischillin@gmail.com>
2019-08-21 23:33:18 -0400

    completed generating random word from array / generate underscores of chosen word langth.

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,6 @@
 // Create an Array of words
 
-var lootWord = ["Oily Rags", "Ancient Sword", "Holy Symbol" ];
+var lootWord = ["OilyRags", "AncientSword", "HolySymbol" ];
 
 // Choose word randomly form array
 
@@ -8,9 +8,24 @@ var chosenWord = Math.floor(Math.random() * lootWord.length);
 var chosenWord = lootWord[chosenWord];
 console.log(chosenWord);
 
+
+//underscores form chosen word will go here
+
+//var underScore = "";
+var underScore = [];
+
 // Create Underscores based on word length
 
+//function createUnderscore() {
+    for (var i = 0; i < chosenWord.length; i++) 
+    {
+        underScore.push("_")
+        //underScore = underScore + "_ ";
+    }
+    
+//}
 
+console.log(underScore);
 // Get Users Guess
 // Check users guess if right
 // If right push to correct array
