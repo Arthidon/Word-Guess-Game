7f822ffc8d09acc711d826f0eba385f62f5d6ce6
Anthony Yonta <atischillin@gmail.com>
2019-08-21 21:40:42 -0400

    created base index file.psuedocode js file. css file

---
+++ assets/script.js
@@ -0,0 +1,17 @@
+// Create an Array of words
+
+var lootWord = ["Oily Rags", "Ancient Sword", "Holy Symbol" ];
+
+// Choose word randomly form array
+
+var chosenWord = Math.floor(Math.random() * lootWord.length);
+var chosenWord = lootWord[chosenWord];
+console.log(chosenWord);
+
+// Create Underscores based on word length
+
+
+// Get Users Guess
+// Check users guess if right
+// If right push to correct array
+// If wrong push to wrong array
